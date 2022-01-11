6bec47a530b98e3bfa88ea07412745eb1380364a
Olek MP <olek.mp23@gmail.com>
2022-01-11 17:27:30 -0500

    Refactor: methods and styles

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//@ts-check
+
+window.onscroll = function () {
+  addClassSticky();
+};
+
+const header = document.getElementById('myHeader');
+const sticky = header.offsetTop;
+
+const addClassSticky = () => {
+  if (window.pageYOffset > sticky) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+};
